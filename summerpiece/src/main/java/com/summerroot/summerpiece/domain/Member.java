eec3a4dfb633957b276e6af167bf7eba18de540e
oos43 <ys0430p@naver.com>
2022-03-26 17:57:39 +0900

    Member 클래스 생성

---
+++ summerpiece/src/main/java/com/summerroot/summerpiece/domain/Member.java
@@ -0,0 +1,24 @@
+package com.summerroot.summerpiece.domain;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private int no;
+
+    private String email;
+    private String pwd;
+    private String name;
+    private String nickname;
+    private String phone;
+    private LocalDateTime enrollDate;
+
+    @Enumerated(EnumType.STRING)
+    private MemberStatus status;
+}
