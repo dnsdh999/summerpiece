089e7f45e631b20c0ef992b8ba483e2da18fc77e
eunhye <eunhye_3@naver.com>
2022-05-02 11:24:16 +0900

    controller

---
+++ summerpiece/src/main/java/com/summerroot/summerpiece/controlller/CalendarController.java
@@ -0,0 +1,9 @@
+package com.summerroot.summerpiece.controlller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Slf4j
+public class CalendarController {
+}
